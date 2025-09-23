03ccc6eaaf48615b6432f5ccb303862717c57868
uzaka5944-lab <uzaka5944@gmail.com>
2025-09-23 16:35:39 +0500

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,19 +8,20 @@ plugins {
 
 android {
     namespace = "com.gymmanagement.app"
-    compileSdk = 34 // Use specific version instead of flutter.compileSdkVersion
+    compileSdk = 34 // Use specific compileSdk version
 
     defaultConfig {
         applicationId = "com.gymmanagement.app"
-        minSdk = 21 // Use specific version instead of flutter.minSdkVersion
-        targetSdk = 34 // Use specific version instead of flutter.targetSdkVersion
-        versionCode = 1 // Hardcode version code
-        versionName = "1.0.0" // Hardcode version name
+        minSdk = 21 // Use specific minSdk version
+        targetSdk = 34 // Use specific targetSdk version
+        versionCode = 1 // Hardcoded version code
+        versionName = "1.0.0" // Hardcoded version name
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true          // Enable code shrinking (required for resource shrinking)
+            isShrinkResources = true        // Enable resource shrinking
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
